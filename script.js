55dfbabc13226f31645fd23a4cbb923cb451d100
Myriam13014 <Myriam.mogni@gmail.com>
2019-08-07 13:42:42 +0200

    dépôt dossier

---
+++ script.js
@@ -0,0 +1,49 @@
+const vols = [{
+  destination: 'Martinique',
+  date_depart: '01/25/2019',
+  compagnie_aerienne: 'Ryanair',
+  numVol: 'xxxxx',
+  prix: '350',
+  dureeTrajet: '6'
+},
+{
+  destination: 'Londres',
+  date_depart: '07/25/2019',
+  compagnie_aerienne: 'Air france',
+  numVol: 'xxxxx',
+  prix: '150',
+  dureeTrajet: '2'
+},
+{
+  destination: 'Paris',
+  date_depart: '08/12/2019',
+  compagnie_aerienne: 'Air france',
+  numVol: 'xxxxx',
+  prix: '50',
+  dureeTrajet: '1'
+},
+{
+  destination: 'Mexico',
+  date_depart: '09/03/2019',
+  compagnie_aerienne: 'Lufthansa',
+  numVol: 'xxxxx',
+  prix: '1350',
+  dureeTrajet: '8'
+},
+{
+  destination: 'Tokyo',
+  date_depart: '05/12/2019',
+  compagnie_aerienne: 'Air france',
+  numVol: 'xxxxx',
+  prix: '750',
+  dureeTrajet: '14'
+},
+{
+  destination: 'Tokyo',
+  date_depart: '02/12/2020',
+  compagnie_aerienne: 'Lufthansa',
+  numVol: 'xxxxx',
+  prix: '850',
+  dureeTrajet: '14'
+}
+];
